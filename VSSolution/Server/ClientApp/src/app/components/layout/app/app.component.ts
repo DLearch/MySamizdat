1dd2f41ec4fd9bea9e3be23b315563e06fdf4da7
DLearch <larch.dn@gmail.com>
2019-01-08 23:50:06 +0200

    Update app.component.ts

--- VSSolution/Server/ClientApp/src/app/components/layout/app/app.component.ts
+++ VSSolution/Server/ClientApp/src/app/components/layout/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { AppConfigService } from 'src/app/services/app-config/app-config.service';
 
 @Component({
   selector: 'app-root',
@@ -6,8 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-
-  constructor() { }
+  
+  constructor(
+    private config: AppConfigService,
+    public translate: TranslateService
+  ) {
+    translate.setDefaultLang(this.config.get('DefaultLang') as string);
+  }
 
   ngOnInit() {
   }
