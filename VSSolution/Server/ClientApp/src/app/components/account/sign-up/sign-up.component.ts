0d474c5068c7f846685ae5a9a6ff6c2395da175e
DLearch <larch.dn@gmail.com>
2019-01-10 21:37:29 +0200

    Update sign-up.component

--- VSSolution/Server/ClientApp/src/app/components/account/sign-up/sign-up.component.ts
+++ VSSolution/Server/ClientApp/src/app/components/account/sign-up/sign-up.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrationService } from 'src/app/services/registration/registration.service';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.css']
+  styleUrls: ['./sign-up.component.css'],
+  providers: [RegistrationService]
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  public signUpForm: FormGroup;
+  public isComplete = false;
+  
+  public constructor(
+    formBuilder: FormBuilder
+    , private registrationService: RegistrationService
+  ) {
 
-  ngOnInit() {
+    this.signUpForm = formBuilder.group({
+
+      'email': ['', [Validators.required, Validators.email]],
+      'name': ['', [Validators.required]],
+      'password': ['', [Validators.required]]
+    });
   }
 
+  ngOnInit() { }
+
+  public register(): void {
+
+    this.registrationService
+      .register(this.signUpForm.value)
+      .subscribe(
+        () => this.handleSignUpResponse(),
+        (data) => this.handleSignUpErrorResponse(data)
+      );
+  }
+
+  private handleSignUpResponse(): void {
+    
+    this.isComplete = true;
+  }
+
+  private handleSignUpErrorResponse(data: any): void { }
 }
