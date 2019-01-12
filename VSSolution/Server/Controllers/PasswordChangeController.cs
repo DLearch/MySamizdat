fc011385c57721d3a078cbaf9cb97ac70412042c
DLearch <larch.dn@gmail.com>
2019-01-12 16:51:15 +0200

    Create PasswordChangeController

---
+++ VSSolution/Server/Controllers/PasswordChangeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+using Server.ViewModels.PasswordChange;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Controllers
+{
+    public class PasswordChangeController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public PasswordChangeController(
+            UserManager<User> userManager
+        )
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Index([FromBody]PasswordChangeVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+                if (user != null)
+                {
+                    IdentityResult result = await _userManager.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+
+                    if (result.Succeeded)
+                        return Ok();
+                    else
+                        foreach (var error in result.Errors)
+                            ModelState.AddModelError(string.Empty, error.Description);
+                }
+                else
+                    ModelState.AddModelError("", "not-found");
+            }
+
+            return BadRequest(ModelState);
+        }
+    }
+}
