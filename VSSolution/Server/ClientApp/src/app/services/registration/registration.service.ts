91a7a2f19035dff14493937e63aeafc16edfc115
DLearch <larch.dn@gmail.com>
2019-01-13 20:50:11 +0200

    Update registration.service.ts

--- VSSolution/Server/ClientApp/src/app/services/registration/registration.service.ts
+++ VSSolution/Server/ClientApp/src/app/services/registration/registration.service.ts
@@ -1,42 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AppConfigService } from '../app-config/app-config.service';
 import { UserStorageService } from '../user-storage/user-storage.service';
-import { Observable, throwError } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ApiService } from '../api/api.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
 
+  private readonly registerController: string = 'registration';
+
   constructor(
-    private config: AppConfigService
+    private api: ApiService
     , userStorage: UserStorageService
-    , private http: HttpClient
   ) {
     userStorage.unsetUser();
   }
 
-  public register(user: any): Observable<any> {
+  public register(data: any): Observable<any> {
 
-    return this.http
-      .post(this.getRegisterUrl(), user)
-      .pipe(
-        map(() => this.handleRegisterResponse()),
-        catchError(response => this.handleRegisterErrorResponse(response))
-      );
-  }
-
-  private getRegisterUrl(): string {
-
-    return this.config.get('PathAPI') + 'registration';
-  }
-
-  private handleRegisterResponse(): void { }
-
-  private handleRegisterErrorResponse(data: any): any {
-
-    return throwError(data);
+    return this.api
+      .post(data, this.registerController);
   }
 }
