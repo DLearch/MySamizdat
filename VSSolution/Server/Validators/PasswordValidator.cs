3f81e504d6ea65be6364c3847ce97df827cd9bf3
DLearch <larch.dn@gmail.com>
2019-01-18 00:52:03 +0200

    Create User and Password validators

---
+++ VSSolution/Server/Validators/PasswordValidator.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Server.Validators
+{
+    public class PasswordValidator : IPasswordValidator<User>
+    {
+        public const string code = "Password";
+        public const string regexPattern = @"(?=^.{6,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$";
+
+        public Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user, string password)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+
+            if (string.IsNullOrEmpty(password))
+                AddError("password-input-error-empty", errors);
+            else if (!Regex.IsMatch(password, regexPattern))
+                AddError("password-input-error-wrong", errors);
+
+            return Task.FromResult(errors.Count == 0 ?
+                IdentityResult.Success : IdentityResult.Failed(errors.ToArray()));
+        }
+
+        private void AddError(string error, List<IdentityError> errors)
+        {
+            errors.Add(new IdentityError
+            {
+                Code = code
+                , Description = error
+            });
+        }
+    }
+}
---
+++ VSSolution/Server/Validators/UserValidator.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Server.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Server.Validators
+{
+    public class UserValidator : IUserValidator<User>
+    {
+        public const string userNameCode = "UserName";
+        public const string emailCode = "Email";
+
+        public const string regexUserNamePattern = @"^[a-zA-Z0-9_]+$";
+
+        public async Task<IdentityResult> ValidateAsync(UserManager<User> manager, User user)
+        {
+            List<IdentityError> errors = new List<IdentityError>();
+
+            if (string.IsNullOrWhiteSpace(user.UserName))
+                AddError(userNameCode, "username-input-error-empty", errors);
+            else if (!Regex.IsMatch(user.UserName, regexUserNamePattern))
+                AddError(userNameCode, "username-input-error-wrong", errors);
+            else if ((await manager.FindByNameAsync(user.UserName)) != null)
+                AddError(userNameCode, "username-input-error-already-taken", errors);
+
+            if (string.IsNullOrWhiteSpace(user.Email))
+                AddError(emailCode, "email-input-error-empty", errors);
+            else if (!(new EmailAddressAttribute().IsValid(user.Email)))
+                AddError(emailCode, "email-input-error-wrong", errors);
+            else if ((await manager.FindByEmailAsync(user.Email)) != null)
+                AddError(emailCode, "email-input-error-already-taken", errors);
+
+            return errors.Count == 0 ?
+                IdentityResult.Success : IdentityResult.Failed(errors.ToArray());
+        }
+
+        private void AddError(string code, string error, List<IdentityError> errors)
+        {
+            errors.Add(new IdentityError
+            {
+                Code = code
+                , Description = error
+            });
+        }
+    }
+}
