e2b7702499d1fda361dfb4b4991435bc18dd7712
DLearch <larch.dn@gmail.com>
2019-01-20 23:35:36 +0200

    Update authentication.service.ts

--- VSSolution/Server/ClientApp/src/app/services/authentication/authentication.service.ts
+++ VSSolution/Server/ClientApp/src/app/services/authentication/authentication.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { UserStorageService } from '../user-storage/user-storage.service';
-import { map, catchError } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { ApiService } from '../api/api.service';
 
 @Injectable({
@@ -24,21 +24,11 @@ export class AuthenticationService {
       .post(data, this.authenticateController)
       .pipe(
         map(response => this.handleAuthenticateResponse(response))
-        , catchError(response => this.handleAuthenticateErrorResponse(response))
       );
   }
 
   private handleAuthenticateResponse(data: any): void {
 
     this.userStorage.setUser(data.name, data.token);
-  }
-  private handleAuthenticateErrorResponse(data: any): any {
-
-    for (let key in data) {
-      data[key.toLowerCase()] = data[key];
-      delete data[key];
-    }
-
-    return throwError(data);
   }
 }
