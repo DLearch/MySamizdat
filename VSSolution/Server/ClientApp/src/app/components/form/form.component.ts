c89aedf15e07ed6550670f22ebd42e5e8e115b76
DLearch <larch.dn@gmail.com>
2019-03-02 18:36:37 +0200

    form

--- VSSolution/Server/ClientApp/src/app/components/form/form.component.ts
+++ VSSolution/Server/ClientApp/src/app/components/form/form.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, ValidationErrors } from '@angular/forms';
+import { ControlTemplate } from './control-template';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent {
 
-  constructor() { }
+  form: FormGroup;
+  controlTemplates: ControlTemplate[];
 
-  ngOnInit() {
+
+
+  @Output() onFormChanged = new EventEmitter<FormGroup>();
+
+  @Input() set apiErrors(value: { [key: string]: string[] }) {
+
+    let adaptedKey: string;
+    for (let key in value) {
+
+      if (key)
+        adaptedKey = key[0].toLowerCase() + key.slice(1);
+      else
+        adaptedKey = key;
+
+      if (this.form.controls[adaptedKey]) {
+
+        let errors: { [key: string]: any } = {};
+
+        if (this.form.controls[adaptedKey].errors)
+          errors = this.form.controls[adaptedKey].errors;
+
+        let apiErrors: string[] = [];
+        for (let error of value[key])
+          apiErrors.push(error);
+
+        errors['api'] = apiErrors;
+
+        this.form.controls[adaptedKey].setErrors(errors);
+      }
+    }
   }
 
+  @Input() set template(value: ControlTemplate[]) {
+
+    let controls: { [key: string]: any } = {};
+
+    for (let controlConfig of value) {
+      controls[controlConfig.name] = ['', controlConfig.validators]
+    }
+
+    this.controlTemplates = value;
+    this.form = this.formBuilder.group(controls);
+    this.form.valueChanges.subscribe(() => this.onFormChanged.emit(this.form))
+  };
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  getErrorTKs(errors: ValidationErrors): string[] {
+
+    const errorTKs: string[] = [];
+
+    for (let errorKey in errors) {
+      switch (errorKey) {
+
+        case 'required':
+          errorTKs.push('empty');
+          break;
+
+        case 'maxlength':
+          errorTKs.push('long');
+          break;
+
+        case 'minlength':
+          errorTKs.push('short');
+          break;
+
+        case 'email':
+          errorTKs.push('wrong');
+          break;
+
+        case 'pattern':
+          errorTKs.push('wrong');
+          break;
+
+        case 'api':
+          for (let error of errors[errorKey])
+            errorTKs.push(error);
+          break;
+      }
+    }
+
+    return errorTKs;
+  }
 }
--- VSSolution/Server/ClientApp/src/app/pages/main/main.component.ts
+++ VSSolution/Server/ClientApp/src/app/pages/main/main.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Validators } from '@angular/forms';
+import { ControlTemplate } from 'src/app/components/form/control-template';
+import { AppValidators } from 'src/app/app-validators';
 
 @Component({
   selector: 'app-main',
@@ -8,10 +11,25 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class MainComponent implements OnInit {
 
+  template: ControlTemplate[];
+
   constructor(
     private translate: TranslateService
   ) {
+    this.template = [];
 
+    this.template.push({
+      name: 'username',
+      tk: 'username',
+      validators: [Validators.required, AppValidators.userName] 
+    });
+    this.template.push({
+      name: 'languageTK',
+      tk: 'language',
+      type: 'select',
+      validators: [Validators.required],
+      selectElements: [{ value: 'ru', tk: 'language.ru' }, { value: 'en', tk: 'language.en' }]
+    });
   }
 
   ngOnInit() {
