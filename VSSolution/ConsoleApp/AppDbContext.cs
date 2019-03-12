95ba26f2a757cc5dc0792d4e7111aa676062a236
DLearch <larch.dn@gmail.com>
2019-03-13 01:15:09 +0200

    ConsoleAPp models

--- VSSolution/ConsoleApp/AppDbContext.cs
+++ VSSolution/ConsoleApp/AppDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace ConsoleApp
@@ -8,157 +10,303 @@ namespace ConsoleApp
     class AppDbContext : DbContext
     {
         public DbSet<User> Users { get; set; }
-        public DbSet<Team> Teams { get; set; }
-        public DbSet<Series> Series { get; set; }
-        public DbSet<Chain> Chains { get; set; }
         public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Bookmark> Bookmarks { get; set; }
+        public DbSet<TranslateBook> TranslateBooks { get; set; }
+        public DbSet<BookComment> BookComments { get; set; }
+        public DbSet<ChapterComment> ChapterComments { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<TeamMemberRole> TeamMemberRoles { get; set; }
+        public DbSet<TeamInviteNotification> TeamInviteNotifications { get; set; }
 
         public AppDbContext()
         {
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<UserTeam>()
-                .HasKey(t => new { t.UserId, t.TeamId });
+            modelBuilder.Entity<BookComment>()
+               .HasOne(c => c.Book)
+               .WithMany(c => c.Comments)
+               .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<UserTeam>()
-                .HasOne(sc => sc.User)
-                .WithMany(s => s.Teams)
-                .HasForeignKey(sc => sc.UserId);
+            modelBuilder.Entity<Bookmark>()
+               .HasOne(c => c.Book)
+               .WithMany(c => c.Bookmarks)
+               .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<UserTeam>()
-                .HasOne(sc => sc.Team)
-                .WithMany(c => c.Members)
-                .HasForeignKey(sc => sc.TeamId);
-            ///////////////////////////////////////////////////////////////////////////////////////
-            modelBuilder.Entity<UserTeam>()
-                .HasKey(t => new { t.UserId, t.TeamId });
+            modelBuilder.Entity<ChapterComment>()
+               .HasOne(c => c.Chapter)
+               .WithMany(c => c.Comments)
+               .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<UserTeam>()
-                .HasOne(sc => sc.User)
-                .WithMany(s => s.Teams)
-                .HasForeignKey(sc => sc.UserId);
+            modelBuilder.Entity<TeamInviteNotification>()
+               .HasOne(c => c.Team)
+               .WithMany(c => c.Invites)
+               .OnDelete(DeleteBehavior.Restrict);
 
-            modelBuilder.Entity<UserTeam>()
-                .HasOne(sc => sc.Team)
-                .WithMany(c => c.Members)
-                .HasForeignKey(sc => sc.TeamId);
+            modelBuilder.Entity<TeamInviteNotification>()
+               .HasOne(c => c.User)
+               .WithMany(c => c.TeamInvites)
+               .OnDelete(DeleteBehavior.Restrict);
 
+            // TranslateBook OriginalLanguage Required
+            modelBuilder.Entity<TranslateBook>()
+                        .HasOne(e => e.OriginalLanguage)
+                        .WithMany(e => e.TranslateBooks)
+                        .HasForeignKey(e => e.OriginalLanguageTK)
+                        .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Language>().HasData(
+                new Language[]
+                {
+                new Language { TK = "en"},
+                new Language { TK = "ru"},
+                new Language { TK = "ua"}
+                });
+
+            modelBuilder.Entity<TeamMemberRole>().HasData(
+                new TeamMemberRole[]
+                {
+                new TeamMemberRole { TK = "head"},
+                new TeamMemberRole { TK = "deputy-head"},
+                new TeamMemberRole { TK = "member"}
+                });
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=runootestdb;Trusted_Connection=True;");
         }
     }
+
     public class User
     {
-        public int Id { get; set; }
-        public string UserName { get; set; }
-        public List<UserTeam> Teams { get; set; }
-        public List<UserBook> BooksSubscribes { get; set; }
-        public List<UserChain> ChainsSubscribes { get; set; }
-        public List<UserSeries> SeriesSubscribes { get; set; }
-        public List<UserTeam> TeamsSubscribes { get; set; }
-    }
-    public class Team
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<UserTeam> Members { get; set; }
-        public List<TeamBook> Books { get; set; }
-        public List<TeamSeries> Series { get; set; }
-        public List<UserTeam> Subscribers { get; set; }
+        [Key]
+        public string Id { get; set; }
 
+        [Required]
+        public string UserName { get; set; }
+
+        public string Email { get; set; }
+
+        public List<Bookmark> Bookmarks { get; set; }
+        
+        public List<BookComment> BookComments { get; set; }
+        public List<ChapterComment> ChapterComments { get; set; }
+
+        public List<Book> Books { get; set; }
+
+        public List<TeamMember> TeamMembers { get; set; }
+
+        public List<TeamInviteNotification> TeamInvites { get; set; }
     }
-    public class Series
-    {
-        public int Id { get; set; }
-        public string Title { get; set; }
-        public List<UserSeries> Subscribers { get; set; }
-        public List<TeamSeries> Team { get; set; }
-        public List<SeriesChain> Chains { get; set; }
-    }
-    public class Chain
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public List<ChainBook> Books { get; set; }
-        public List<UserChain> Subscribers { get; set; }
-        public List<SeriesChain> Series { get; set; }
-    }
+
     public class Book
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+
+        [Required]
         public string Title { get; set; }
-        public List<UserBook> Subscribers { get; set; }
-        public List<TeamBook> Team { get; set; }
-        public List<ChainBook> Chains { get; set; }
-    }
 
-    public class UserTeam
-    {
-        public int UserId { get; set; }
+        public string Description { get; set; }
+
+        public string CoverPath { get; set; }
+        
+        public string AuthorName { get; set; }
+        public Author Author { get; set; }
+
+        [Required]
+        public string LanguageTK { get; set; }
+        public Language Language { get; set; }
+
+        public List<Chapter> Chapters { get; set; }
+
+        public List<Bookmark> Bookmarks { get; set; }
+
+        public List<BookComment> Comments { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
         public User User { get; set; }
 
-        public int TeamId { get; set; }
+        public string TeamName { get; set; }
         public Team Team { get; set; }
+
+        public string Discriminator { get; set; }
     }
-    public class UserBook
+
+    public class Author
     {
-        public int UserId { get; set; }
-        public User User { get; set; }
+        [Key]
+        public string Name { get; set; }
+
+        public List<Book> Books { get; set; }
+    }
+
+    public class Chapter
+    {
+        [Key]
+        public int Id { get; set; }
 
         public int BookId { get; set; }
         public Book Book { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+
+        public string Content { get; set; }
+        public List<ChapterComment> Comments { get; set; }
     }
-    public class UserChain
+
+    public class Language
     {
-        public int UserId { get; set; }
+        [Key]
+        public string TK { get; set; }
+
+        [InverseProperty("Language")]
+        public List<Book> Books { get; set; }
+
+        [InverseProperty("OriginalLanguage")]
+        public List<TranslateBook> TranslateBooks { get; set; }
+    }
+    
+    public class Bookmark
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string UserId { get; set; }
         public User User { get; set; }
 
-        public int ChainId { get; set; }
-        public Chain Chain { get; set; }
-    }
-    public class UserSeries
-    {
-        public int UserId { get; set; }
-        public User User { get; set; }
-
-        public int SeriesId { get; set; }
-        public Series Series { get; set; }
-    }
-    public class TeamSeries
-    {
-        public int TeamId { get; set; }
-        public Team Team { get; set; }
-
-        public int SeriesId { get; set; }
-        public Series Series { get; set; }
-    }
-    public class TeamBook
-    {
-        public int TeamId { get; set; }
-        public Team Team { get; set; }
-
+        [Required]
         public int BookId { get; set; }
         public Book Book { get; set; }
     }
-    public class SeriesChain
-    {
-        public int SeriesId { get; set; }
-        public Series Series { get; set; }
 
-        public int ChainId { get; set; }
-        public Chain Chain { get; set; }
+    public class TranslateBook : Book
+    {
+        [Required]
+        public string OriginalTitle { get; set; }
+        
+        [Required]
+        public string OriginalLanguageTK { get; set; }
+        public Language OriginalLanguage { get; set; }
+
     }
-    public class ChainBook
-    {
-        public int ChainId { get; set; }
-        public Chain Chain { get; set; }
 
+    public class Comment
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public string Discriminator { get; set; }
+    }
+    public class BookComment : Comment
+    {
+        [Required]
         public int BookId { get; set; }
         public Book Book { get; set; }
+    }
+    public class ChapterComment : Comment
+    {
+        [Required]
+        public int ChapterId { get; set; }
+        public Chapter Chapter { get; set; }
+    }
+
+    public class Team
+    {
+        [Key]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+        
+        public string Description { get; set; }
+
+        public List<TeamMember> TeamMembers { get; set; }
+        public List<TeamInviteNotification> Invites { get; set; }
+        public List<Book> Books { get; set; }
+    }
+
+    public class TeamMember
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+
+        [Required]
+        public string TeamName { get; set; }
+        public Team Team { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        [Required]
+        public string TeamMemberRoleTK { get; set; }
+        public TeamMemberRole TeamMemberRoles { get; set; }
+
+        public List<TeamInviteNotification> Invites { get; set; }
+    }
+
+    public class TeamMemberRole
+    {
+        [Key]
+        public string TK { get; set; }
+
+        public List<TeamMember> TeamMembers { get; set; }
+    }
+
+    public class Notification
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+
+        [Required]
+        public bool IsChecked { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public string Discriminator { get; set; }
+    }
+
+    public class TeamInviteNotification : Notification
+    {
+        [Required]
+        public string TeamName { get; set; }
+        public Team Team { get; set; }
+
+        [Required]
+        public int TeamMemberId { get; set; }
+        public TeamMember TeamMember { get; set; }
     }
 }
--- VSSolution/ConsoleApp/Program.cs
+++ VSSolution/ConsoleApp/Program.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp
         {
             using (AppDbContext db = new AppDbContext())
             {
+                ///////////////////////////////////////////////////////////////////////////////////
+
                 // создаем два объекта User
                 User user1 = new User { UserName = "User1" };
                 User user2 = new User { UserName = "User2" };
@@ -17,7 +19,7 @@ namespace ConsoleApp
                 db.Users.Add(user1);
                 db.Users.Add(user2);
                 db.SaveChanges();
-                Console.WriteLine("Объекты успешно сохранены");
+                Console.WriteLine("User успешно сохранены");
 
                 // получаем объекты из бд и выводим на консоль
                 var users = db.Users.ToList();
@@ -26,6 +28,168 @@ namespace ConsoleApp
                 {
                     Console.WriteLine($"{u.Id}.{u.UserName}");
                 }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                
+                Author author1 = new Author { Name = "Author1" };
+                Author author2 = new Author { Name = "Author2" };
+
+                db.Author.Add(author1);
+                db.Author.Add(author2);
+                db.SaveChanges();
+                Console.WriteLine("Author успешно сохранены");
+
+
+                // получаем объекты из бд и выводим на консоль
+                var authors = db.Author.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Author b in authors)
+                {
+                    Console.WriteLine($"{b.Name}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                Team team1 = new Team() { Name = "Team1Name", CreationTime = DateTime.Now };
+
+                db.Teams.Add(team1);
+                db.SaveChanges();
+                Console.WriteLine("Teams успешно сохранены");
+
+                var teams = db.Teams.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Team b in teams)
+                {
+                    Console.WriteLine($"{b.Name}.{b.CreationTime}");
+                }
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                Book book1 = new Book { Title = "Book1", LanguageTK = "ru", User = user1, TeamName = "Team1Name" };
+                Book book2 = new Book { Title = "Book2", LanguageTK = "en", AuthorName = "Author1", User = user2 };
+
+                db.Books.Add(book1);
+                db.Books.Add(book2);
+                db.SaveChanges();
+                Console.WriteLine("Book успешно сохранены");
+
+
+                // получаем объекты из бд и выводим на консоль
+                var books = db.Books.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Book b in books)
+                {
+                    Console.WriteLine($"{b.Id}.{b.Title}.{b.AuthorName}.{b.LanguageTK}.{b.User.UserName}.{b.TeamName}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                
+                Bookmark bookmark1 = new Bookmark { BookId = book1.Id, UserId = user1.Id };
+                Bookmark bookmark2 = new Bookmark { BookId = book2.Id, UserId = user2.Id };
+
+                db.Bookmarks.Add(bookmark1);
+                db.Bookmarks.Add(bookmark2);
+                db.SaveChanges();
+                Console.WriteLine("Bookmark успешно сохранены");
+
+
+                // получаем объекты из бд и выводим на консоль
+                var bookmarks = db.Bookmarks.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Bookmark b in bookmarks)
+                {
+                    Console.WriteLine($"{b.Id}.{b.User.UserName}.{b.BookId}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                TranslateBook tbook1 = new TranslateBook { Title = "TBook1", LanguageTK = "ru", OriginalLanguageTK = "en", OriginalTitle = "ORGTBook1", User = user1 };
+                TranslateBook tbook2 = new TranslateBook { Title = "TBook2", LanguageTK = "en", OriginalLanguageTK = "ua", AuthorName = "Author2", OriginalTitle = "ORGTBook2", User = user1 };
+
+                db.TranslateBooks.Add(tbook1);
+                db.Books.Add(tbook2);
+                db.SaveChanges();
+                Console.WriteLine("TranslateBooks успешно сохранены");
+
+
+                // получаем объекты из бд и выводим на консоль
+                var tbooks = db.TranslateBooks.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (TranslateBook b in tbooks)
+                {
+                    Console.WriteLine($"{b.Id}.{b.Title}.{b.AuthorName}.{b.LanguageTK}.{b.OriginalLanguageTK}.{b.OriginalTitle}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                BookComment bookComment1 = new BookComment() { UserId = user1.Id, BookId = book1.Id, CreationTime = DateTime.Now, Content = "content1" };
+                BookComment bookComment2 = new BookComment() { UserId = user2.Id, BookId = book2.Id, CreationTime = DateTime.Now, Content = "content2" };
+
+                db.BookComments.Add(bookComment1);
+                db.BookComments.Add(bookComment2);
+                db.SaveChanges();
+                Console.WriteLine("BookComments успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var bookComments = db.BookComments.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (BookComment b in bookComments)
+                {
+                    Console.WriteLine($"{b.Id}.{b.User.UserName}.{b.BookId}.{b.Discriminator}.{b.CreationTime}.{b.Content}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                Chapter chapter1 = new Chapter() { Content = "Content1", BookId = book1.Id};
+                Chapter chapter2 = new Chapter() { Content = "Content2", BookId = book1.Id };
+
+                db.Chapters.Add(chapter1);
+                db.Chapters.Add(chapter2);
+                db.SaveChanges();
+                Console.WriteLine("Chapters успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var chapters = db.Chapters.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (Chapter b in chapters)
+                {
+                    Console.WriteLine($"{b.Id}.{b.Content}.{b.BookId}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                ChapterComment chapterComment1 = new ChapterComment() { UserId = user1.Id, ChapterId = chapter1.Id, CreationTime = DateTime.Now, Content = "content1" };
+                ChapterComment chapterComment2 = new ChapterComment() { UserId = user2.Id, ChapterId = chapter2.Id, CreationTime = DateTime.Now, Content = "content2" };
+
+                db.ChapterComments.Add(chapterComment1);
+                db.ChapterComments.Add(chapterComment2);
+                db.SaveChanges();
+                Console.WriteLine("ChapterComments успешно сохранены");
+
+                // получаем объекты из бд и выводим на консоль
+                var chapterComments = db.ChapterComments.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (ChapterComment b in chapterComments)
+                {
+                    Console.WriteLine($"{b.Id}.{b.User.UserName}.{b.ChapterId}.{b.Discriminator}.{b.CreationTime}.{b.Content}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+
+                TeamMember teamMember1 = new TeamMember() { TeamName = team1.Name, UserId = user1.Id, CreationTime = DateTime.Now, TeamMemberRoleTK = "head" };
+
+                db.TeamMembers.Add(teamMember1);
+                db.SaveChanges();
+                Console.WriteLine("TeamMembers успешно сохранены");
+
+                var teamMembers = db.TeamMembers.ToList();
+                Console.WriteLine("Список объектов:");
+                foreach (TeamMember b in teamMembers)
+                {
+                    Console.WriteLine($"{b.Id}.{b.TeamName}.{b.User.UserName}.{b.TeamMemberRoleTK}");
+                }
+
+                ///////////////////////////////////////////////////////////////////////////////////
+                
             }
             Console.Read();
         }
