dc3c80223b4a22ed16cc83bd410623f93a419a56
DLearch <larch.dn@gmail.com>
2019-11-14 21:33:54 +0100

    Create EmailSender

---
+++ Server/Services/EmailSenders/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using MailKit;
+using Microsoft.Extensions.Configuration;
+using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services.EmailSenders
+{
+    public class EmailSender : IEmailSender
+    {
+        public string Host => Configuration["Email:Host"];
+        public int Port => int.Parse(Configuration["Email:Port"]);
+        public bool UseSSL => bool.Parse(Configuration["Email:UseSSL"]);
+        public string Address => Configuration["Email:Address"];
+        public string Password => Configuration["Email:Password"];
+
+        private IConfiguration Configuration { get; set; }
+        private IMailTransport MailTransport { get; set; }
+
+        public EmailSender(
+            IConfiguration configuration,
+            IMailTransport mailTransport
+        )
+        {
+            Configuration = configuration;
+            MailTransport = mailTransport;
+        }
+
+        public void Send(MimeMessage message)
+        {
+            MailTransport.Connect(Host, Port, UseSSL);
+            MailTransport.Authenticate(Address, Password);
+            MailTransport.Send(message);
+            MailTransport.Disconnect(true);
+        }
+
+        public async Task SendAsync(MimeMessage message)
+        {
+            await MailTransport.ConnectAsync(Host, Port, UseSSL);
+            await MailTransport.AuthenticateAsync(Address, Password);
+            await MailTransport.SendAsync(message);
+            await MailTransport.DisconnectAsync(true);
+        }
+    }
+}
---
+++ Server/Services/EmailSenders/IEmailSender.cs
@@ -0,0 +1,15 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Services.EmailSenders
+{
+    public interface IEmailSender
+    {
+        void Send(MimeMessage message);
+
+        Task SendAsync(MimeMessage message);
+    }
+}
