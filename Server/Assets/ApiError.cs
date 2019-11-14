656d490318568c7bab8e00d0c99d5ae1477c0c38
DLearch <larch.dn@gmail.com>
2019-11-14 13:35:03 +0100

    Create ApiError.cs

---
+++ Server/Assets/ApiError.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Assets
+{
+    public enum ApiError
+    {
+        Wrong,
+        NotFound,
+        AlreadyCompleted,
+        AlreadyTaken,
+        Empty,
+        NotCompleted
+    }
+}
