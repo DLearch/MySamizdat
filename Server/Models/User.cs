53b524cb3a88c545870ca4507d37d355b00179e6
DLearch <larch.dn@gmail.com>
2019-11-14 21:32:59 +0100

    Create User.cs

---
+++ Server/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Server.Models
+{
+    public class User : IdentityUser, ICreated
+    {
+        [Required]
+        public bool EmailIsVisible { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public string AvatarPath { get; set; }
+
+        [Required]
+        public DateTime CreationTime { get; set; }
+    }
+}
