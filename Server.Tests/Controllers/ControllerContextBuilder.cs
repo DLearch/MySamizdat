f7048c3a3e47b6f2a6fb349c4199cda5d7bffdcd
DLearch <larch.dn@gmail.com>
2019-11-19 11:19:04 +0100

    Create ControllerContextBuilder.cs

---
+++ Server.Tests/Controllers/ControllerContextBuilder.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text;
+
+namespace Server.Tests.Controllers
+{
+    public class ControllerContextBuilder
+    {
+        public ControllerContext ControllerContext { get; private set; }
+        private ClaimsIdentity ClaimsIdentity { get; set; }
+        public ControllerContextBuilder()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            ClaimsIdentity = new ClaimsIdentity();
+            var claimsPrincipal = new ClaimsPrincipal(ClaimsIdentity);
+            ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext() { User = claimsPrincipal }
+            };
+        }
+
+        public void AddClaim(Claim claim)
+        {
+            ClaimsIdentity.AddClaim(claim);
+        }
+    }
+}
